6f69b6cd257b0b7a5e4a32785d8fc0a3cc5719b9
Thespyinthehole <joshuaaubrey@talktalk.net>
2021-02-12 15:25:57 +0000

    Implemented the store function

--- app/Http/Controllers/Event/EventSessionsController.php
+++ app/Http/Controllers/Event/EventSessionsController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Session;
 
 class EventSessionsController extends Controller
 {
@@ -41,10 +42,48 @@ class EventSessionsController extends Controller
      * @param int $id
      * @return Response
      */
-    public function store(Request $request, int $id): Response
+    public function store(Request $request, int $id)
     {
+        $this->validateSession($request);
+
+        $event = Event::findOrFail($id);
+        $session = $this->populateSession(new Session(), $request);
+
+
+        $session->event_id = $id;
+
+        $session->save();
         // Given a question and answer, creates and associates a new response with the provided session.
         // Accepts requests from the user that owns the session.
-        return response()->noContent();
+        return new SessionResource($session);
+    }
+
+    /**
+     * Validates the incoming request.
+     *
+     * @param Request $request
+     * @throws ValidationException
+     */
+    protected function validateSession(Request $request)
+    {
+        $this->validate($request, [
+            'user_id' => 'required|exists:users,id',
+            'started_at' => 'required|date',
+        ]);
+    }
+
+    /**
+     * Populates an session with data from the provided request.
+     *
+     * @param Session $session
+     * @param Request $request
+     * @return Session|mixed
+     */
+    protected function populateSession(Session $session, Request $request): Session
+    {
+        return tap($session, function (Session $session) use ($request) {
+            $session->user_id = $request->input('user_id');
+            $session->started_at = $request->input('started_at');
+        });
     }
 }
