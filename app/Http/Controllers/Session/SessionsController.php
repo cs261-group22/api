3550dbf274621e2065f018112a8c7ed93a7f7b84
Thespyinthehole <joshuaaubrey@talktalk.net>
2021-02-16 16:56:21 +0000

    Implemented the show session method

--- app/Http/Controllers/Session/SessionsController.php
+++ app/Http/Controllers/Session/SessionsController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Session;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
+use App\Models\Event;
+use App\Models\Session;
+use App\Http\Resources\SessionResource;
+use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
@@ -13,10 +17,18 @@ class SessionsController extends Controller
      * @param int $id
      * @return Response
      */
-    public function show(int $id): Response
+    public function show(int $id)
     {
+        $user = Auth::user();
+        $session = Session::findOrFail($id);
+        $event = $session->event;
+
+        // Only admins and event hosts can view the sessions for the event
+        if (! $user->is_admin || ! $event->hostedByUser($user)) {
+            return response('You are not authorized to view this session', 403);
+        }
         // Retrieves information about the specified session.
         // Accepts requests from the user that owns the session.
-        return response()->noContent();
+        return new SessionResource($session);
     }
 }
