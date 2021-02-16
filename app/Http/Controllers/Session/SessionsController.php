46b103b3166cafb17848ff8636a256b36f136467
Thespyinthehole <joshuaaubrey@talktalk.net>
2021-02-16 17:05:06 +0000

    Linted ready to merge

--- app/Http/Controllers/Session/SessionsController.php
+++ app/Http/Controllers/Session/SessionsController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Session;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Response;
-use App\Models\Session;
 use App\Http\Resources\SessionResource;
+use App\Models\Session;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
@@ -21,7 +21,7 @@ class SessionsController extends Controller
         $user = Auth::user();
         $session = Session::findOrFail($id);
         // Only admins and event hosts can view the sessions for the event
-        if ($user->id!==$session->user_id) {
+        if ($user->id !== $session->user_id) {
             return response('You are not authorized to view this session', 403);
         }
         // Retrieves information about the specified session.
