be2dbf7761ad1184fec231033b78cdbc6298fbff
Callum Bloomfield <callum.bloomfield01@gmail.com>
2021-03-01 00:52:24 +0000

    Add mood field to session model

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Session extends Model
 {
@@ -16,6 +17,7 @@ class Session extends Model
      * @var array
      */
     protected $fillable = [
+        'mood',
         'user_id',
         'event_id',
         'started_at',
