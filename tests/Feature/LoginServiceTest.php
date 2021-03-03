ef54c3b4401b665cbe2eb86f97ca55bcfcc598d2
Thespyinthehole <joshuaaubrey@talktalk.net>
2021-03-03 15:33:04 +0000

    Implemented both non admin password fail and non admin email fail

--- tests/Feature/LoginServiceTest.php
+++ tests/Feature/LoginServiceTest.php
@@ -70,8 +70,9 @@ class LoginServiceTest extends TestCase
         $response = $this->postJson('/api/v1/login/employee', ['email' => $email . '1', 'password' => $password]);
         $response->assertStatus(401);
     }
+
     /**
-     * A test for logging in as admin with wrong email
+     * A test for logging in as non admin
      *
      * @return void
      */
@@ -90,5 +91,42 @@ class LoginServiceTest extends TestCase
 
         $validResponse = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/api/v1/users');
         $validResponse->assertStatus(403);
+    }
+
+
+    /**
+     * A test for logging in as non admin with wrong password
+     *
+     * @return void
+     */
+    public function testFailedPasswordNonAdminLogin()
+    {
+        $email = 'admin@example.com';
+        $password = 'password';
+
+        $user = User::factory()->non_admin()->create([
+            'email' => $email,
+            'password' => Hash::make($password)
+        ]);
+        $response = $this->postJson('/api/v1/login/employee', ['email' => $email, 'password' => $password . '1']);
+        $response->assertStatus(401);
+    }
+
+    /**
+     * A test for logging in as non admin with wrong email
+     *
+     * @return void
+     */
+    public function testFailedEmailNonAdminLogin()
+    {
+        $email = 'admin@example.com';
+        $password = 'password';
+
+        $user = User::factory()->non_admin()->create([
+            'email' => $email,
+            'password' => Hash::make($password)
+        ]);
+        $response = $this->postJson('/api/v1/login/employee', ['email' => $email . '1', 'password' => $password]);
+        $response->assertStatus(401);
     }
 }
