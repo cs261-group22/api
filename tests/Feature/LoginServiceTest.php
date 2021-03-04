d79de9305745a729770a48ba6c80cfd9d84030c4
Thespyinthehole <joshuaaubrey@talktalk.net>
2021-03-04 13:07:08 +0000

    Implemented Guest Login test

--- tests/Feature/LoginServiceTest.php
+++ tests/Feature/LoginServiceTest.php
@@ -129,4 +129,20 @@ class LoginServiceTest extends TestCase
         $response = $this->postJson('/api/v1/login/employee', ['email' => $email . '1', 'password' => $password]);
         $response->assertStatus(401);
     }
+
+    /**
+     * A test for logging in as guest
+     *
+     * @return void
+     */
+    public function testSuccessfulGuestLogin()
+    {
+
+        $response = $this->post('/api/v1/login/guest');
+        $response->assertStatus(200)->assertJsonStructure(['token']);
+        $token = $response['token'];
+
+        $validResponse = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/api/v1/users');
+        $validResponse->assertStatus(403);
+    }
 }
