fef5d7ae3b681b6b47a77b5847439a3942070ac6
Callum Bloomfield <callum.bloomfield01@gmail.com>
2021-03-14 16:05:31 +0000

    Remove unused code

--- config/cs261.php
+++ config/cs261.php
@@ -5,7 +5,6 @@ return [
     'pwa' => [
         'url'                     => env('CS261_PWA_URL', 'localhost:3030'),
         'email_verification_path' => env('CS261_PWA_EMAIL_VERIFICATION_PATH', '/verify-email'),
-        'email_password_reset'    => env('CS261_PWA_PASSWORD_RESET_PATH', '/password-reset'),
     ],
     'analytics' => [
         'mock' => (bool) env('CS261_ANALYTICS_MOCK', true),
